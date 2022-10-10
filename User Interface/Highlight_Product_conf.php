d45c5d78056911e28494dd6caa804beb8c915348
Outlen <112525452+Outlen@users.noreply.github.com>
2022-10-10 17:31:30 +1100

    Add files via upload

---
+++ User Interface/Highlight_Product_conf.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <meta name="description" content="Adds members from database" />
+    <meta name="author" content="Kipp STRATMANN" />
+    <title>GotoGrow-MRM Member Management</title>
+    </head>
+<body>
+    <header>
+        <?php   
+        /* add_member_conf.php
+        Adds members to the SQL database. 
+        Author: K. Stratmann
+        Last Edited: 19/09/2022
+        */
+        ?>
+        <link href="styles/style.css" rel="stylesheet" />
+    </header>
+    <h1>GotoGrow-MRM Website</h1>
+<nav>
+    <ul>
+    <li class="menu"><a href="index.php">Home</a></li>
+        <li class="menu"><a href="check_add.php">Add Member</a></li>
+        <li class="menu"><a href="check_edit.php">Edit Member</a></li>
+        <li class="menu"><a href="check_delete.php">Delete Member</a></li>
+        <li class="menu"><a href="check_sales_add.php">Add Sales</a></li>
+        <li class="menu"><a href="check_sales_edit.php">Edit Sales</a></li>
+        <li class="menu"><a href="check_sales_delete.php">Delete Sales</a></li>
+        <li class="menu"><a href="check_item_add.php">Add item</a></li>
+        <li class="menu"><a href="check_item_edit.php">Edit item</a></li>
+        <li class="menu"><a href="check_item_delete.php">Delete item</a></li>
+		<li class="menu"><a href="check_view_details.php">View member</a></li>
+    </ul>		
+</nav>
+<hr> 
+    <?php
+    //Starts the session
+    session_start();
+
+    //Delcearse the rest of the vairables to be used
+    $month = $_POST["month"];
+	$year = $_POST["year"];
+    $servername = "localhost";
+	$user = "root";
+	$pwd = "";
+	$sql_db = "goto_gro_databases";
+    //Makes the connection to the database
+    $conn = new mysqli($servername, $user, $pwd, $sql_db);
+
+    //Checks that an error message doesn't exits and  increments the count value 
+	$results = $conn->query("SELECT stockName, SKU, SUM(itemQuantityBought) as BoughtQuantity
+								FROM sales NATURAL JOIN STOCK
+								WHERE monthname(date) = '$month' AND year(date) = '$year'
+								GROUP BY stockName
+								ORDER BY BoughtQuantity DESC");
+
+	echo "<table border='1'>
+
+		<tr>
+		<th>stockName</th>
+		<th>SKU</th>
+		<th>BoughtQuantity</th>
+		</tr>";
+	while($row = mysqli_fetch_array($results)) {
+  
+		echo "<tr>";
+  
+		echo "<td>" . $row['stockName'] . "</td>";
+		echo "<td>" . $row['SKU'] . "</td>";
+		echo "<td>" . $row['BoughtQuantity'] . "</td>";
+  
+		echo "</tr>";
+  }
+  
+	echo "</table>";	
+	
+	//Close the connection
+	$conn->close();
+            
+        
+    ?>  
+    <footer>
+    <hr>
+        <p>Site Designed and Created by: MSP32</p> 
+</footer>
+
+
+</body>
+ 
+
+
+</html>
